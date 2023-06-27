da77bdc04b1a6d27d533f7b7375b32e7764042cf
Madison Jackson <madi.jackson29@gmail.com>
2023-06-27 11:23:19 -0700

    added pseudo to board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,14 @@
 // change card list to take in an argument (board id)
 // add a selector to App that toggles which boards display
-// card list only gets 
+// card list only gets cards for selected board
 
 import CardList from './components/CardList';
 import cardData from './data/cards.json';
+
+//cardlist should fetch cards with specific board_id
+//boardlistJSX (own component?) - radio form in app to choose what board to display
+//cardlist only produces cards === board.id
+//board.id needs to be saved in state
 
 const Board = () => {
     return (
