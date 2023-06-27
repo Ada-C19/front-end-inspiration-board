a7f4dbf08927c053af5f62e42cacbeb8912014fc
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-06-27 12:29:19 -0400

    passed down card props

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Card from './Card';
 
-const CardList = (props) => {
-  console.log('CL props:', props)
-  
+const CardList = ({ cardBoardData }) => {
+  // console.log('CL props:', props)
+
+  const cardListComponents = cardBoardData.map(card => {
+    
+    return (
+      <li>
+      <Card
+      id = {card.id}
+      message = {card.message}
+      likesCount = {card.likesCount}
+      />
+      </li>
+    )
+  })
+
   return (
-    <div>CardList</div>
+    <ul>
+      {cardListComponents}
+    </ul>
+  )
+}
+
+CardList.propTypes = {
+  card: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+      likesCount: PropTypes.number.isRequired
+    })
   )
 }
 
