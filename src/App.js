d15b1de6387def728d0cb414087b6b6e1fa8d593
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-06-27 12:09:18 -0400

    adds CardList component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import CardList from './components/CardList.js'
+
+const cardBoardData = [
+  {
+    "id": 1,
+    "message": "This is a quote",
+    "likesCount": 0
+  },
+  {
+    "id": 2,
+    "message": "This is a cooler quote",
+    "likesCount": 0
+  }
+]
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CardList cardBoardData={cardBoardData} />
       </header>
     </div>
   );
