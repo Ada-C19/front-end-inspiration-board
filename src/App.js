d12758ed895da289390381617d4d3a696094d1ad
nadia <reach.nadia.saria@gmail.com>
2023-06-29 11:57:07 -0700

    commit changes before branching for API calls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 import CardList from './components/CardList';
 import Board from './components/Board';
@@ -9,13 +10,41 @@ import BoardSelectRadio from './components/BoardSelectRadio';
 import boardData from './data/boards.json';
 // use ${process.env.REACT_APP_BACKEND_URL} to make API calls
 
+const boardsURL = `${process.env.REACT_APP_BACKEND_URL}`
+
+const getAllBoards = () => {
+  return axios.get(`${boardsURL}/boards`)
+  .then((response) => {
+    return (response.data.map(convertBoardFromAPI))})
+  .catch((e) => console.log(e));
+}
+
+const convertBoardFromAPI = (data) => {
+    return {
+      id: data.board_id,
+      title: data.title,
+      owner: data.owner
+    };
+};
+
+const getCardsForBoard = (boardId) => {
+  return axios.get(`${boardsURL}/boards/${boardId}/cards`)
+    .then((response) => console.log(response.data.cards))
+}
 
 const App = () => {
   const [boards, setBoards] = useState(boardData);
   const [targetBoardId, setTargetBoardId] = useState(1);
 
+  // const fetchBoardData = () => {
+  //   getAllBoards().then((boards) => setBoards(boards))
+  // }
+
+  // useEffect( () => {fetchBoardData()}, [])
+
   const handleSelectBoard = (boardId) => {
     setTargetBoardId(boardId);
+    // getCardsForBoard(boardId);
   };
 
   const findIndexOfTargetBoard = () => {
@@ -65,7 +94,7 @@ const App = () => {
       </header>
       <main>
         <BoardSelectRadio boards={boards} onBoardSelect={handleSelectBoard} />
-        <Board board_id={targetBoardId} />
+        {/* <Board board_id={targetBoardId} /> */}
         {displayCards(targetBoardId)}
         <NewCardForm addCard={handleSubmitCard} />
       </main>
