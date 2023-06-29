1e2556e08e79889552cc06501edcc255ab8ebf8f
Kat <kmunson13@gmail.com>
2023-06-29 11:07:57 -0400

    Fix card form so all children aligned

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,35 +1,32 @@
-import React, { useState} from 'react';
-// import './CardForm.css';
+import React, { useState } from "react";
+import './CardForm.css';
 // import PropTypes from 'prop-types';
-import Card from './Card';
+import Card from "./Card";
 
 const CardForm = ({ addCardCallback }) => {
-    const [formFields, setFormFields] = React.useState({
-      message: '',
-      likeCount: 0
-    });
+  const [formFields, setFormFields] = React.useState({
+    message: "",
+    likeCount: 0,
+  });
 
-    // return (
-    //     <div className="card-form">
-    //         Create a new card:
-    //         {CardForm}
-    //     </div>
-    //     )
+  // return (
+  //     <div className="card-form">
+  //         Create a new card:
+  //         {CardForm}
+  //     </div>
+  //     )
 
-    return (
-        <form onSubmit={formFields} className='card-form'>
-            <div className='new-card-form'>
-                <label htmlFor="cardMessage">Message:</label>
-                <input
-                    name="cardMessage"
-                    value={formFields.message}
-                    onChange={setFormFields} />
-            </div>
-            <input
-                type="submit"
-                value="Create New Card" />
-        </form>
-    );
+  return (
+    <form onSubmit={formFields} className="card-form">
+      <label htmlFor="cardMessage">Message:</label>
+      <input
+        name="cardMessage"
+        value={formFields.message}
+        onChange={setFormFields}
+      />
+      <input type="submit" value="Create New Card" />
+    </form>
+  );
 };
 
 // const handleSubmit = (event) => {
