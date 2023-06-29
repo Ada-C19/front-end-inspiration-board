d7886118509de880e2c018e50ba5e9614ec3c8f9
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-06-29 19:07:15 -0400

    created onClick event for delete button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Card = ({ id, message, likesCount, increaseLikeCount }) => {
+const Card = ({ id, message, likesCount, deleteCard }) => {
   console.log('Card props:', message)
+
   return (
-    <ul>
-      <li>
+    <li>
       Quote: {message}
-      </li>
-      <li>
-      Likes: {likesCount}
-        <button onClick={() => increaseLikeCount(id)}>+1</button>
-      </li>
-    </ul>
+      Likes: {likesCount} 💕
+      <button>
+        +1
+      </button>
+      <button onClick={() => deleteCard(id)}>
+        Delete
+      </button>
+    </li>
   )
-}
+};
 
 Card.propTypes = {
   id: PropTypes.number.isRequired,
   message: PropTypes.string.isRequired,
   likesCount: PropTypes.number.isRequired
-}
+};
 
-export default Card
+export default Card;
