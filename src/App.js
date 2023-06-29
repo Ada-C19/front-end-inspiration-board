e5ae46a08a42f64d0f430db504f15f0dc3aa47b3
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-06-29 18:47:28 -0400

    created deleteCard function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Board from './components/Board';
 import BoardList from './components/BoardList';
 
 
-const cardBoardData = [
+const cardData = [
   {
   "id": 1,
   "message": "This is a quote",
@@ -20,47 +20,45 @@ const boardData = [
   {
   "id": 1,
   "title": "Title 1",
-  // "likesCount": 0
+  "owner": "Owner 1"
   },
   {
   "id": 2,
   "title": "Title 2",
-  // "likesCount": 0
+  "owner": "Owner 2"
   }
 ]
 function App() {
-  const [likes, setLikes] = useState(cardBoardData)
+  const [cards, setCards] = useState(cardData)
+  const [likes, setLikes] = useState(0)
   const [selectedBoard, setSelectedBoard] = useState(null)
+
+  console.log('cardData.likesCount', cardData)
+
   const boardSelect = (id, title) => {
       setSelectedBoard({id, title})
   }
 
-  const increaseLikeCount = id => {
-    setLikes(prevLikes => {
-      const updateLikes = prevLikes.map(like => {
-        return like.id === id ? {...like, likesCount: + 1} : like 
-      });
-      return updateLikes;
-    });
-   
-  
-  
-  // const increaseLikes = (id) => {
-  //   setMessages(prevMessages => {
-  //     const updatedMessages = prevMessages.map(message => {
-  //       return message.id  === id ?  {...message, liked: !message.liked} : message;
-  //     });
-  //     return updatedMessages;
-  //   });
-  // };
+  const deleteCard = id => {
+    console.log('click delete')
+    setCards(currentCards => {
+      const updatedCards =  currentCards.filter(card => card.id !== id);
+      return updatedCards
+    })
+  }
 
-  console.log(selectedBoard)
   return (
     <div className="App">
       <header className="App-header">Inspiration Board</header>
       <main>
-        <BoardList boardSelect={boardSelect} boardData={boardData}/>
-        <Board cardBoardData={cardBoardData}/>
+        <BoardList 
+        boardSelect={boardSelect} 
+        boardData={boardData}
+        />
+        <Board 
+        cardData={cards} 
+        deleteCard={deleteCard}
+        />
       </main>
     </div>
   );
