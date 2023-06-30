0f7c809aacabf1a6636b940864d7ab27f98c4a78
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-06-29 22:30:24 -0400

    created addBoard function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import './App.css';
 import React, { useState } from 'react';
 import Board from './components/Board';
 import BoardList from './components/BoardList';
+import NewBoardForm from './components/NewBoardForm';
 
+
+// ######## Dummy Data ######## 
+
+const boardData = [
+  {
+  "id": 1,
+  "title": "Title 1",
+  "owner": "Owner 1"
+  },
+  {
+  "id": 2,
+  "title": "Title 2",
+  "owner": "Owner 2"
+  }
+]
 
 const cardData = [
   {
@@ -16,28 +32,38 @@ const cardData = [
   "likesCount": 0
   }
 ]
-const boardData = [
-  {
-  "id": 1,
-  "title": "Title 1",
-  "owner": "Owner 1"
-  },
-  {
-  "id": 2,
-  "title": "Title 2",
-  "owner": "Owner 2"
-  }
-]
+
 function App() {
   const [cards, setCards] = useState(cardData)
+  const [boardsData, setBoardsData] = useState(boardData)
   const [likes, setLikes] = useState(0)
   const [selectedBoard, setSelectedBoard] = useState(null)
 
   console.log('cardData.likesCount', cardData)
 
+  // ######## Select Board######## 
+  // Need to fix this.
+
   const boardSelect = (id, title) => {
       setSelectedBoard({id, title})
   }
+
+  // ######## Add new board ######## 
+
+  const addBoard = newBoard => {
+    const newBoardList = [...boardsData];
+    const nextId = crypto.randomUUID();
+    console.log('nextID', nextId)
+
+    newBoardList.push({
+      id: nextId,
+      title: newBoard.title,
+      owner: newBoard.owner
+    })
+    setBoardsData(newBoardList)
+  };
+
+// ######## Delete individual card ######## 
 
   const deleteCard = id => {
     console.log('click delete')
@@ -53,15 +79,19 @@ function App() {
       <main>
         <BoardList 
         boardSelect={boardSelect} 
-        boardData={boardData}
+        boardData={boardsData}
+        selectedBoard={selectedBoard}
         />
         <Board 
         cardData={cards} 
         deleteCard={deleteCard}
         />
+        <NewBoardForm 
+        addBoard={addBoard}
+        />
       </main>
     </div>
   );
-}
+};
 
 export default App;
