1bbd76947d11ece41868158870fba49772e2a9d2
Yael Peet <yaelpeet@gmail.com>
2023-06-30 11:44:31 -0700

    file structure, component structure functioning

---
+++ src/components/NewBoardForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewBoardForm = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+
+export default NewBoardForm;
---
+++ src/components/NewCardForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewCardForm = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+
+export default NewCardForm;
