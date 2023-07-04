ebb10ec30d2d42a363eaacf81a8a25ed3292a1e9
Setharika Sok <soksetharika7@gmail.com>
2023-07-04 16:04:10 -0400

    Created event handling for card for

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import './CardForm.css';
-// import PropTypes from 'prop-types';
-import Card from "./Card";
+import PropTypes from 'prop-types';
+// import Card from "./Card";  
 
 const CardForm = ({ addCardCallback }) => {
-  const [formFields, setFormFields] = React.useState({
-    message: "",
-    likeCount: 0,
-  });
+    const [formFields, setFormFields] = useState({
+        message: "",
+        likes_count: 0,
+    }); 
+
+    const onCardChange = (event) => {
+        setFormFields({
+        ...formFields,
+        message: event.target.value,
+        });
+    }; 
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        addCardCallback(formFields);
+
+        setFormFields({ 
+            message: '',
+            likes_count: 0,
+        });
+    };
 
   // return (
   //     <div className="card-form">
@@ -17,12 +35,13 @@ const CardForm = ({ addCardCallback }) => {
   //     )
 
   return (
-    <form onSubmit={formFields} className="card-form">
+    <form onSubmit={handleSubmit} className="card-form">
       <label htmlFor="cardMessage">Message:</label>
       <input
         name="cardMessage"
         value={formFields.message}
-        onChange={setFormFields}
+        // onChange={setFormFields}
+        onChange={onCardChange}
       />
       <p className="warning-message">Message must be less than 40 characters</p>
       <input className="card-submit-button" type="submit" value="Create New Card" />
@@ -30,15 +49,9 @@ const CardForm = ({ addCardCallback }) => {
   );
 };
 
-// const handleSubmit = (event) => {
-//     event.preventDefault();
-
-//     addCardCallback(formFields);
-
-//     setFormFields({
-//         message: '',
-//         likeCount: 0
-//     });
-// };
+CardForm.propTypes = {
+    addCardCallback: PropTypes.func.isRequired,
+};
 
 export default CardForm;
+  
