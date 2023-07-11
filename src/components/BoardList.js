2a60ad94af1c37271944fd1a511727b50c9869d1
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-11 19:09:40 -0400

    refactored

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-function BoardList({ boardData, boardSelect, increaseLikeCount }) {
+function BoardList({ boardData, boardSelect }) {
     const boardTitles = boardData.map(board => {
-        return <li onClick={() => boardSelect(board.id, board.title)}>{board.title}</li>
+        return (
+        <li 
+          onClick={() => boardSelect(board.id, board.title)}
+        >
+          {board.title}
+        </li>
+        )
     })
+    
     return (
         <ol>{boardTitles}</ol>
     )
