a5a1a14f88457cb53d7aa735ec01ae2337600016
halimAremu <halimat3000@gmail.com>
2023-07-11 14:28:25 -0500

    more styling

--- src/components/BoardForm.js
+++ src/components/BoardForm.js
@@ -6,12 +6,14 @@ const AddBoard = (props) => {
     return (
     <form className='NewBoardForm'>
         <h3 id='boardtitle'>Create New Board</h3>
+        <div className='input'>
         <label htmlFor="name">Title</label>
         <input type="text" id="name" name='name' ></input>
         <label htmlFor="owner">Owner's Name</label>
         <input type="text" id="owner" name='owner' ></input>
         <input type="submit" value="Submit"></input>
         <button className='button'>Hide New Board Form</button>
+        </div>
     </form>
     )}
 
