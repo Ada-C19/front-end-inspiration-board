c493c66920768efd5485013cd949eb39f7d7c879
halimAremu <halimat3000@gmail.com>
2023-07-11 16:56:45 -0500

    hide form complete

--- src/components/BoardForm.js
+++ src/components/BoardForm.js
@@ -1,20 +1,27 @@
 import React, {useState} from 'react';
 import './BoardForm.css';
 
-
 const AddBoard = (props) => {
+    const [hide, setHide] = useState(false)
+    const handleHideBoard = (event) =>{
+        setHide(current => !current)
+    }
+
+    const hidden = hide? 'hide-form':""
     return (
-    <form className='NewBoardForm'>
-        <h3 id='boardtitle'>Create New Board</h3>
-        <div className='input'>
+    <section>
+    <h3 id='boardtitle'>Create New Board</h3>
+    <form className={hidden}>
+        <div className= 'input'>
         <label htmlFor="name">Title</label>
         <input type="text" id="name" name='name' ></input>
         <label htmlFor="owner">Owner's Name</label>
         <input type="text" id="owner" name='owner' ></input>
         <input type="submit" value="Submit"></input>
-        <button className='button'>Hide New Board Form</button>
         </div>
     </form>
+    <button className='button' onClick={handleHideBoard}>Hide New Board Form</button>
+    </section>
     )}
 
 export default AddBoard;
