f168b7ed7a9879327dff941012392fa109589216
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-17 16:27:26 -0400

    added key and refactored

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -4,19 +4,17 @@ import './BoardList.css';
 function BoardList({ boardData, boardSelect }) {
     const boardTitles = boardData.map(board => {
         return (
-        <ul
+        <ul key = {board.board_id}
           onClick={() => boardSelect(board.board_id, board.title, board.owner)}
         >
           {board.title}
         </ul>
-        )
-    })
+        );
+    });
     
     return (
-      <>
-        <ol className='list-boards'>{boardTitles}</ol>
-      </>
-    )
-}
+      <ol className='list-boards'>{boardTitles}</ol>
+    );
+};
 
 export default BoardList;
