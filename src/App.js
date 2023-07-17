96a8f2690ec283c678ecf8d42aac152e2380e8eb
Paige <ph.paigee@gmail.com>
2023-07-17 15:01:19 -0400

    set up initial get request in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NewBoardForm from './components/NewBoardForm';
 import './App.css';
 import BoardList from './components/BoardList';
 import axios from 'axios';
 
+export const URL = 'http://localhost:5000/boards'
+
 const App = () => {
+  const [boards, setBoards] = useState([]);
+  const [boardData, setBoardData] = useState({
+    boardId: '',
+    title: '',
+    owner: ''
+  });
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        const newBoards = response.data.map((board) => ({
+          boardId: board.boardId,
+          title: board.title,
+          owner: board.owner,
+        }));
+        setBoards(newBoards);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
 
 
   return (
