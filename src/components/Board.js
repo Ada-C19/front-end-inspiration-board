6dfd87eba78a78b8092fd9d3750fd767cbad3c4e
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-17 16:43:11 -0400

    removed cardSelect and added proptypes

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import CardList from './CardList';
+import PropTypes from 'prop-types';
 
 
-function Board({ cardData, increaseLikesCount, deleteCard, cardSelect}) {
+function Board({ cardData, increaseLikesCount, deleteCard }) {
 
-    return (
-        <div>
-          <CardList 
-          cardData={cardData} 
-          deleteCard={deleteCard}
-          increaseLikesCount={increaseLikesCount}
-          cardSelect={cardSelect}
-          />
-        </div>
-    )
-}
+  return (
+    <div>
+      <CardList 
+      cardData={cardData} 
+      deleteCard={deleteCard}
+      increaseLikesCount={increaseLikesCount}
+      />
+    </div>
+  );
+};
+
+Board.propTypes = {
+  cardData: PropTypes.array.isRequired,
+  increaseLikesCount: PropTypes.func.isRequired,
+  deleteCard: PropTypes.func.isRequired
+};
 
 export default Board;
