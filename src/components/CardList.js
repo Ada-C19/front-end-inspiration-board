57dead75ae5f64fe6ce687f3fa38d5ef001ca42f
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-17 16:34:50 -0400

    refactored

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Card from './Card';
 import './CardList.css';
@@ -6,35 +6,36 @@ import './CardList.css';
 const CardList = ({ cardData, increaseLikesCount, deleteCard }) => {
 
   const cardListComponents = cardData.map(card => {
-    
     return (
-      <ul key = {card.id}>
+      <ul key = {card.card_id}>
         <Card
-        id = {card.id}
-        message = {card.message}
-        likesCount = {card.likesCount}
-        increaseLikesCount={increaseLikesCount}
-        deleteCard={deleteCard}
+          id = {card.card_id}
+          message = {card.message}
+          likesCount = {card.likes_count}
+          increaseLikesCount={increaseLikesCount}
+          deleteCard={deleteCard}
         />
       </ul>
-    )
-  })
+    );
+  });
 
   return (
     <ul className='card-list-container'>
       {cardListComponents}
     </ul>
-  )
-}
+  );
+};
 
 CardList.propTypes = {
   card: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      message: PropTypes.string.isRequired,
-      likesCount: PropTypes.number.isRequired
-    })
+    PropTypes.shape(
+      {
+        id: PropTypes.number.isRequired,
+        message: PropTypes.string.isRequired,
+        likesCount: PropTypes.number.isRequired
+      }
+    )
   )
-}
+};
 
 export default CardList;
