c276989669f06b15067ea5ae354afb50bd1aa778
Gloria <gloria.wan.glory@gmail.com>
2023-07-17 11:53:29 -0400

    beginning to create Board and Card components

---
+++ src/components/BoardList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import Board from './Board';
+
+const BoardList = (boards) => {
+    return (
+        <div>BoardList</div>
+    )
+}
+
+export default BoardList
