ea24d613d8abc308aea83efbb71d880251cd6166
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-17 16:11:15 -0400

    refactored and set useState to empty string

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,31 +1,20 @@
 import React, { useState } from 'react';
 
 const NewCardForm = ({ addCard }) => {
-  const [newCard, setNewCards] = useState(
-    {
-      message: '',
-      likesCount: 0
-    }
-  );
+  const [newCard, setNewCards] = useState('');
 
   const handleSubmit = event => {
-    event.preventDefault()
+    event.preventDefault();
 
-    console.log('submit')
     if (newCard.message.length > 40) {
-      alert('Message too long')
+      alert('Max 40 characters. Try again.');
     } else {
-      addCard(
-        {
-          message: newCard.message,
-          likesCount: newCard.likesCount
-        })
+      addCard(newCard.message);
     };
 
     setNewCards(
       {
         message: '',
-        likesCount: 0
       }
     );
   };
@@ -36,27 +25,26 @@ const NewCardForm = ({ addCard }) => {
         ...newCard, [event.target.name]: event.target.value
       }
     );
-    console.log('newCard:', newCard)
   };
-
-
-
 
   return (
     <section>
       <form onSubmit={handleSubmit}>
-        <label>Card Message:
+        <label>Message:
           <input
             value={newCard.message}
-            name='message'
             onChange={handleChange}
+            name='message'
             type='text'
           />
         </label>
-        <input type='submit' value='Submit'/>
+        <input 
+          type='submit' 
+          value='Submit'
+        />
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default NewCardForm
+export default NewCardForm;
