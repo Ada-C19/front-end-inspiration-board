774cc333b2290c2a8491e57c319fa9ef342b6a4a
halimAremu <halimat3000@gmail.com>
2023-07-17 17:44:26 -0500

    error handling

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -9,7 +9,7 @@ return (
         <h3>Create New Card</h3>
         <form >
         <label htmlFor="message">Message</label> <br />
-        <input type="text" id="message" name="message" pattern='.{1,40}' required ></input> <br />
+        <input type="text" id="message" name="message" pattern='.{1,4}' required ></input> <br />
         <input type="submit" value="Submit"></input>
         <p>Preview:</p>
         </form>
