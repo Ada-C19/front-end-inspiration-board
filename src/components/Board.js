fdfef77dfb28b717f72280c6d21e918969b9f362
abbymachines <abby.machines@gmail.com>
2023-07-17 16:29:41 -0700

    sketch out props for Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,19 @@
 const Board = props => {
   // what is in a board?
-  // cards,
-  return <p>hello board :^)</p>;
+  // board id, owner, title
+  // as well as a container for all the cards
+
+  return(
+  <div>
+    <p>hello board :^)</p>
+    <p>board id: {props.board_id}</p>
+    <p>owner: {props.owner}</p>
+    <p>title: {props.title}</p>
+
+    <p><Card /></p>
+
+  </div>
+  )
 };
 
 export default Board;
