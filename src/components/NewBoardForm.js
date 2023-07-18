dc925f783470e68d83c2592ae07403608841dcfe
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-07-18 17:36:12 -0400

    removed css import

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import './NewBoardForm.css';
 
 const NewBoardForm = ({ addBoard }) => {
   const [newBoard, setNewBoards] = useState(
