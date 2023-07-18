f01f35debd033137e2ffec6fdd61d04bf0eb43f8
Johanna Cabanban <johanna.cabanban@gmail.com>
2023-07-18 15:38:30 -0400

    Board state, SelectedBoard component, handleBoardSelection, handleBoardClick added

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,21 +3,26 @@ import PropTypes from 'prop-types';
 
 const Board = (props) => {
 
-    const toggleBoardSelection = () => {
-        props.setSelectedBoard(!props.selectedBoard);
-      };
+    // const toggleBoardSelection = (boardId) => {
+    //     props.setSelectedBoard(!props.selectedBoard);
+    //   };
     
+    const handleBoardClick = () => {
+        props.onSelectBoard(props.boardId);
+      };
 
     return (
         <li>
             <h3>{props.title}</h3>
-            <button onClick={toggleBoardSelection}>Select Board</button>
+            <button onClick={handleBoardClick}>Select Board</button>
         </li>
     );
 };
 
 Board.propTypes = {
+    boardId: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
+    owner: PropTypes.string.isRequired,
 };
 
 export default Board;
