e81062cd9a04f70ad05e2671cc6bc00c5c1b8daa
Agnes Kim <agneskim4@gmail.com>
2023-07-18 15:06:37 -0400

    changes to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,13 +8,15 @@ function Card ({message, id ,likeCount, increaseLikes, deleteCard}) {
     return (
     <div className="card">
         <h3 className="card-message">
-            Message: {message}
+            {message}
         </h3>
-        <section>
-            Likes: {likeCount}
-        </section>
-        <button className="card-button" onClick={()=> increaseLikes(id)}>+1</button>
-        <button className="card-button" onClick ={()=> deleteCard(id)}> Delete </button>
+        <div className="likes-count">
+            <section >
+                Likes: {likeCount}
+            </section>
+            <button className="card-button" onClick={()=> increaseLikes(id)}>+1</button>
+            <button className="card-button" onClick ={()=> deleteCard(id)}> Delete </button>
+        </div>
     </div>
     )
 }
