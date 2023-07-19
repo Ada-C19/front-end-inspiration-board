ed896f1672137704e5af64496a6bcac8b77a06d6
Jessica <Yiskah.Scientia@gmail.com>
2023-07-19 16:24:59 -0400

    Fix mini board size/style, add border, change selected board to not display

--- src/components/BoardSelectRadio.js
+++ src/components/BoardSelectRadio.js
@@ -1,45 +1,3 @@
-// import React from 'react';
-// import { useState } from 'react';
-// import PropTypes from 'prop-types';
-
-
-// const BoardSelectRadio = ({boards, onBoardSelect}) => {
-//     // dont use index, use ID
-//     const onPickBoard = (event) => {
-//         let boardId = Number(event.target.value);
-//         onBoardSelect(boardId);
-//     };
-
-//     const getBoardListJSX = boards => {
-//         return boards.map((board) => {
-//             return <div>
-//                 <input
-//                 type='radio'
-//                 name='board'
-//                 id={board.title}
-//                 value={board.id}
-//                 key={board.id}
-//                 form='select-board'
-//                 onClick={onPickBoard}
-//                 />
-//             <label htmlFor={board.title}>{board.title}</label>
-//             </div>
-//         });
-//     };
-
-//     return (
-//         <form id='select-board'>
-//             <fieldset form='select-board'>
-//             <legend>Select a board to display:</legend>
-//             <ul>{getBoardListJSX(boards)}</ul>
-//             </fieldset>
-//         </form>
-//     )
-// };
-
-
-// export default BoardSelectRadio;
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import Board from './Board';
@@ -50,7 +8,7 @@ const BoardSelectRadio = ({ boards, onBoardSelect, selectedBoardId }) => {
         onBoardSelect(boardId);
     };
 
-    const filteredBoards = boards.filter((board) => board.id !== selectedBoardId);
+    const filteredBoards = boards.filter((board) => board.id !== Number(selectedBoardId));
 
     return (
         <div className="board-select-container">
@@ -64,7 +22,6 @@ const BoardSelectRadio = ({ boards, onBoardSelect, selectedBoardId }) => {
                     board_id={board.id}
                     title={board.title}
                     owner={board.owner}
-                    deleteBoard={() => {}} // Placeholder function for deleteBoard, since it's not needed here
                 />
             </div>
         ))}
