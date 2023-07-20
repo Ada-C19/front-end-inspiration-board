654ec3a82644ed7b3bbd43fed9d84cf5291c44e8
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-07-19 20:13:08 -0700

    fixed functionality to map over cards in board and added functionality to hide sidebar

--- src/App.js
+++ src/App.js
@@ -29,7 +29,10 @@ const kBaseUrl = 'http://127.0.0.1:5000';
 
 function App() {
   const [boardData, setBoardData] = useState([]);
+  const [cardData, setCardData] = useState([])
   const [selectedBoard, setSelectedBoard] = useState(2);
+  const [isSidebarShown, setIsSidebarShown] = useState(true)
+  
   useEffect(() => {
     axios
       .get(`${kBaseUrl}/boards`)
@@ -37,11 +40,17 @@ function App() {
     .catch((err) => console.log(err));
 }, []);
 
+  useEffect(() => {
+    axios
+      .get(`${kBaseUrl}/boards/${selectedBoard}/cards`)
+      .then((res) => setCardData(res.data))
+      .catch((err) => console.log(err))
+  }, [selectedBoard]);
+
 const handleBoardSubmit = (newBoardData) => {
   axios
     .post(`${kBaseUrl}/boards`, newBoardData)
     .then((res) => {
-
       setBoardData((prev) => [...prev, res.data.board]);
     })
     .catch((err) => console.log(`Failed to assign card to board. Error: ${err}`));
@@ -63,18 +72,25 @@ const handleCardSumbit = (newCardFormProps) => {
     });
 };
 
+const toggleSidebar = (prev) => {
+  setIsSidebarShown((prev) => !prev);
+}
+
 return (
   <div className="App">
-    <p>hello world :)</p>
-    <BoardList data={boardData} setSelectedBoard={setSelectedBoard}/>
-    <Board /*board_id={1}
-      owner={"Abby"}
-      title={"tamagotchis i have loved"}
-      cards={test_board.cards}*/
-      selectedBoard={selectedBoard}
-      baseUrl={kBaseUrl} />
-    <Sidebar handleBoardSubmit={handleBoardSubmit} />
-    <NewCardForm boardId={DEFAULT_BOARD_ID} handleCardSumbit={handleCardSumbit} />
+    <h1 className="App-header">✨Inspiration Board✨</h1>
+    <main className="board-container">
+      <BoardList data={boardData} setSelectedBoard={setSelectedBoard}/>
+      <Board
+        cardData={cardData}
+        // onLike={handleLike}
+      />
+      </main>
+      <div className="sidebar">
+        {isSidebarShown && <Sidebar handleBoardSubmit={handleBoardSubmit} />}
+        <NewCardForm boardId={DEFAULT_BOARD_ID} handleCardSumbit={handleCardSumbit} />
+      </div>
+      <button onClick={toggleSidebar}>Show Sidebar</button>
   </div>
 );
 }
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,34 +1,20 @@
 import './Board.css';
 import Card from '../Card/Card';
-import axios from 'axios';
 // import PropTypes from 'prop-types'
 
 const Board = props => {
-  console.log(props.selectedBoard);
-  const getAllCards = () => {
-    if (props.selectedBoard > 0) {
-    axios
-      .get(`${props.baseUrl}/boards/${props.selectedBoard}/cards`)
-      .then((res) => console.log(res.data))
-  };
-  }
-  getAllCards()
+  console.log(props.cardData)
   return(
-    <div>
-      {/* <p>hello board :^)</p>
-      <p>board id: {props.board_id}</p>
-      <p>owner: {props.owner}</p>
-      <p>title: {props.title}</p> */}
-      
-      {/* { props.cards.map((card) => (
+    <section>
+      { props.cardData.map((card) => (
         <Card 
           message={card.message}
           likes_count={card.likes_count}
           key={card.id}
           />
           ))
-      }  */}
-    </div>
+      } 
+    </section>
   )
 };
 
