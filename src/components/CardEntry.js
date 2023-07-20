927a2c5af54ba50857ca11ac21dcf18fbb8e4e28
Jackie <jackiemay08@gmail.com>
2023-07-20 13:49:17 -0400

    -

--- src/components/CardEntry.js
+++ src/components/CardEntry.js
@@ -2,15 +2,11 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CardEntry = (props) => {
-
-    // const updateLikesCount = (cardId) => {
-    //     props.
-    // }
  
     return (
         <div className={props.cardId}>
             <h2>Card message: {props.message}</h2>
-            <button onClick={(e) => props.onLikeCard}>Likes💕 {props.likesCount}</button>
+            <button onClick={(e) => props.onLikeCard(props.cardId)}>Likes💕 {props.likesCount}</button>
             <button onClick={(e) => props.onUnregister(props.cardId)}>Delete</button>
         </div>
     );
