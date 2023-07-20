cd128445ee128202815f7a7e89e13b81e0a4cb36
Arianna Walden <nedlawa@gmail.com>
2023-07-19 17:53:15 -0700

    fix load issue with initial state of selectedBoard

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const kBaseUrl = 'http://127.0.0.1:5000';
 
 function App() {
   const [boardData, setBoardData] = useState([]);
-  const [selectedBoard, setSelectedBoard] = useState(1);
+  const [selectedBoard, setSelectedBoard] = useState(2);
   useEffect(() => {
     axios
       .get(`${kBaseUrl}/boards`)
