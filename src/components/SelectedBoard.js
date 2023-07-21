410edaa639f606110a333f768c19347a8379d438
Jackie <jackiemay08@gmail.com>
2023-07-21 12:20:03 -0400

    styling

--- src/components/SelectedBoard.js
+++ src/components/SelectedBoard.js
@@ -6,7 +6,7 @@ const SelectedBoard = (props) => {
 
     return (
         <main className="selectedBoard">
-            <h3>Current Selected Board:</h3>
+            <h2>Current Selected Board:</h2>
             <h3>{props.boardState ? props.boardState.title : 'None Selected'}</h3>
         </main>
     );
