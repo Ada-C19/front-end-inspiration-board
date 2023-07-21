7edad2ace7d8f2ce4128d40f6ca2b2c04a493ba5
Yael Peet <yaelpeet@gmail.com>
2023-07-21 11:52:51 -0700

    conditional rendering and noice beta

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './Card.css';
 
 const Card = ({card, deleteCard, addOneLikeToCard, removeOneLikeToCard}) => {
-    
+    const noiceAlert = () => { 
+        if (card.likes_count === 69 )
+        { alert('noice')};
+    };
+
+
     return (<div className="card-container" id="swaying-image">
                 <h3 id='card-item__message'>{card.message}</h3>
-                <p>{card.likes_count} likes</p>
+                {card.likes_count >= 0 ? <p>{card.likes_count} likes</p> : <p>{card.likes_count} dislikes</p> }
+                {noiceAlert()}
                 <section className='card-buttons'>
                     <span onClick={() => removeOneLikeToCard(card)}>💔</span>
                     <span onClick={() => addOneLikeToCard(card)}>❤️</span>
