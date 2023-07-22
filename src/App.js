4a2cbbd36f9327f4b6b226cd602124f0f6a0bf37
Cindy Vides <civides1@gmail.com>
2023-07-22 12:40:38 -0700

    unfinished board component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import BoardList from './components/BoardList';
+import Board from './components/Board';
 import './App.css';
 import axios from 'axios';
 
 const App = () => {
   const [boards, setBoards] = useState([]); 
 // boards is a piece of state
+// setBoards is a function
+  const [selectedBoard, setSelectedBoard] = useState([]);
+// create a state for selected board 
+
   useEffect(()=>{ 
     fetchBoardData().then((boardList)=>{setBoards(boardList)})
   },[])
 
-  
+
 
   return (
     <div className="App">
-      <BoardList boards={boards}>
-
-      </BoardList>
+      <BoardList boards={boards}></BoardList>
+      {boards.map((board) => (
+      <Board
+        key={board.board_id}
+        board_id={board.board_id}
+        title={board.title}
+      />
+    ))}
     </div>
   );
 }
@@ -30,4 +40,21 @@ const fetchBoardData = () => {
   )
 }
 
+  // Event handler to update the selectedd board
+  const handleBoardUpdate = (event) => {
+    const selectedBoardId = event.target.value;
+    const selectedBoard = boards.find((board);
+    setSelectedBoard(selectedBoard);
+  };
 
+
+return (
+  <div className="App">
+    <BoardList boards={boards} />
+    
+      {/* {boards.map((board) => (
+      <Board
+        board_id={board.board_id}
+        title={board.title} */}
+  </div>
+);
---
+++ src/components/Board.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Board = ({ board_id, title, cards}) => {
+
+  return (
+    <select onChange={handleBoardChange}>
+    </select>
+  )
+
+export default Board;
+
+// class Board(db.Model):
+// board_id = db.Column(db.Integer, primary_key=True)
+// title = db.Column(db.String, nullable=False)
+// owner = db.Column(db.String, nullable=False)
+// cards = db.relationship("Card", back_populates="board")
