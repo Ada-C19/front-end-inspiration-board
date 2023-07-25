0793646c9dbf9113a30c14e3c0c76e340867fd90
Cindy Vides <civides1@gmail.com>
2023-07-25 07:13:11 -0700

    attempt to split AddCardData into two parts: an API call and a function within the App component

--- src/App.js
+++ src/App.js
@@ -26,6 +26,41 @@ const App = () => {
   }, [selectedBoard])
   
 
+  
+  // const handleAddCard = (newMessage) => {
+  //   const updatedCards = [...selectedCards];
+  //   updatedCards.push(response.data);
+  //   setSelectedCards(updatedCards);
+
+// should I rename this function?
+  const handleAddCard = (newMessage) => {
+    if (!selectedBoard.board_id) 
+    return ;
+  
+    addCardToBoard(selectedBoard.board_id, newMessage)
+      .then((newCard) => {
+        const updatedCards = [...selectedCards, newCard];
+        setSelectedCards(updatedCards);
+      })
+
+
+// const updatedCards = [...selectedCards];
+// updatedCards.push(response.data);
+// setSelectedCards(updatedCards);
+
+// --- react-chatlog code ---
+// const handleLikeCount = (id) => {
+//   const copyMessages = [...messages];
+//   let newLiked;
+//   for (let i = 0; i < copyMessages.length; i++){
+//     if (copyMessages[i].id === id){
+//       const oneMessage = {...copyMessages[i]}
+//       oneMessage.liked = ! oneMessage.liked; 
+//       copyMessages[i] = oneMessage;
+//       newLiked = oneMessage.liked;
+//     }
+//   }
+
 
   return (
     <div className="App">
@@ -57,11 +92,36 @@ const fetchCardData = (board_id) => {
   )
 }
 
+// or should I rename this one?
 const handleAddCard = (newMessage) => {
   axios.post(`${process.env.REACT_APP_BACKEND_URL}/boards/${selectedBoard.board_id}/cards`, {message: newMessage})
-    .then((response) => {
-      const updatedCards = [...selectedCards];
-      updatedCards.push(response.data);
-      setSelectedCards(updatedCards);
-    })
-};
+    .then((response)  => {return response.data})
+  }
+
+// --- I need to break up this code into two parts ---
+// const handleAddCard = (newMessage) => {
+//   axios.post(`${process.env.REACT_APP_BACKEND_URL}/boards/${selectedBoard.board_id}/cards`, {message: newMessage})
+//     .then((response) => {
+//       const updatedCards = [...selectedCards];
+//       updatedCards.push(response.data);
+//       setSelectedCards(updatedCards);
+//     })
+// };
+
+
+
+// Your handleAddCard is doing two things at the moment.
+// * 		It's making the API call (like fetchBoardData), and
+// * 		It's acting as callback given to the form, 
+// which also needs access to the setter for the card list in order to update the state.
+
+// I'd suggest splitting those two concerns. 
+// Leave the API call in a function outside the App component (like the fetch functions), 
+// that returns a promise to the new card (in response.data). 
+
+// And move the actual handleAddCard into the App component, 
+// like the useEffect functions (but not in an effect). 
+// It can make use of the API wrapper to add the card, 
+// then perform the spread copy and setting using the result. 
+// It will be able to see the current state value and the setter 
+// since the function will be within the component scope.
